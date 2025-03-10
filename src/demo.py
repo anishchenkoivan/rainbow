37f359e168312f9b2c0241ab0eb8a30b45ac8b83
Ivan Anishchenko <ivananisenko08@gmail.com>
2025-03-11 02:07:05 +0300

    Fix nodes initialization

--- src/demo.py
+++ src/demo.py
@@ -9,4 +9,10 @@ class ExampleSceneLoader(SceneLoader):
     @typing.override
     def spawn_nodes(self):
         amount = config.RESOLUTION[0] * config.RESOLUTION[1]
-        return [Node(1.0, 0 if i % config.RESOLUTION[0] * (i // config.RESOLUTION[1]) == 0 else i / amount, 0.0, True if i % config.RESOLUTION[0] * (i // config.RESOLUTION[1]) == 0 else False) for i in range(amount)]
+        nodes = []
+        for i in range(amount):
+            x = i % config.RESOLUTION[0]
+            y = i // config.RESOLUTION[0]
+            border = x * y == 0 or x == config.RESOLUTION[0] - 1 or y == config.RESOLUTION[1] - 1
+            nodes.append(Node(1.0, 0 if border else 0.5, 0.0, border))
+        return nodes
