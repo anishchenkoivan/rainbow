8531bde849ee4dec6974d8b945d99238647bb16c
iv4n-t3a <iv4n.t3a@yandex.ru>
2025-03-11 12:35:30 +0300

    Add transition between wednesdays

--- src/demo.py
+++ src/demo.py
@@ -12,3 +12,19 @@ class ExampleSceneLoader(SceneLoader):
             y == 0 or x == config.RESOLUTION[0] - \
             1 or y == config.RESOLUTION[1] - 1
         return Node(1.0, 0 if border else 0.5, 0.0, border)
+
+
+class TransitionSceneLoader(SceneLoader):
+    def __init__(self, shader_program: ShaderProgram):
+        super().__init__(shader_program)
+
+    @typing.override
+    def get_node(self, x, y):
+        centre_x = config.RESOLUTION[0] // 2
+        centre_y = config.RESOLUTION[1] // 2
+        r = min(config.RESOLUTION) / 5
+
+        if (x - centre_x) ** 2 + (y - centre_y)**2 <= r**2:
+            return Node(5.0, 0.0, 0.0, False)
+
+        return Node(1.0, 10.0 if x == 0 else 0, 0.0, False)
